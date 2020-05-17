fcf465f53643abf337a4133519108348ded91071
kondo1846923 <kondo1846923@gmail.com>
2020-05-17 21:19:55 +0900

    .NET frameworkのバージョン検索アプリ作成

---
+++ GetDotNetVersion_Win32/GetDotNetVersion_Win32/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Win32;
+
+public class GetDotNetVersion_Win32
+{
+    public static void Main(string[] args)
+    {
+        Get45PlusFromRegistry();
+    }
+
+    private static void Get45PlusFromRegistry()
+    {
+        const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+        using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+        {
+            if (ndpKey != null && ndpKey.GetValue("Release") != null)
+            {
+                Console.WriteLine($".NET Framework Version: {CheckFor45PlusVersion((int)ndpKey.GetValue("Release"))}\n続行するには何かキーを押してください ．．．");
+            }
+            else
+            {
+                Console.WriteLine(".NET Framework Version 4.5 or later is not detected.\n続行するには何かキーを押してください ．．．");
+            }
+            Console.ReadLine();
+        }
+
+        string CheckFor45PlusVersion(int releaseKey)
+        {
+            if (releaseKey >= 528040)
+                return "4.8 or later";
+            if (releaseKey >= 461808)
+                return "4.7.2";
+            if (releaseKey >= 461308)
+                return "4.7.1";
+            if (releaseKey >= 460798)
+                return "4.7";
+            if (releaseKey >= 394802)
+                return "4.6.2";
+            if (releaseKey >= 394254)
+                return "4.6.1";
+            if (releaseKey >= 393295)
+                return "4.6";
+            if (releaseKey >= 379893)
+                return "4.5.2";
+            if (releaseKey >= 378675)
+                return "4.5.1";
+            if (releaseKey >= 378389)
+                return "4.5";
+            return "No 4.5 or later version detected";
+        }
+    }
+}
